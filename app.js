8db8bedd1e24cac92783124b53d87921239d1c1e
Trev <trevor.pawlewicz@gmail.com>
2017-03-09 10:44:10 -0500

    added view sections

---
+++ app.js
@@ -0,0 +1,3 @@
+// our base module that bootstraps the application: ng-app in index.html
+//                     [dependencies] that seperate out the functionality
+angular.module('app', ['app.routes', 'app.core']);
